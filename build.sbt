bbaed5f058c2653a285f872bd31a5e0d79d22f2f
Samuel Gélineau <gelisam+github@gmail.com>
2015-08-28 08:06:13 -0400

    find the signature of forall in the scaladocs

--- build.sbt
+++ build.sbt
@@ -2,5 +2,7 @@ lazy val root = (project in file(".")).
   settings(
     name := "failfast-generator",
     version := "0.1.0",
+    scalaVersion := "2.11.7",
+    libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.5",
     doctestSettings
   )
--- src/main/scala/com/gelisam/failfast-generator/ScaladocParser.scala
+++ src/main/scala/com/gelisam/failfast-generator/ScaladocParser.scala
@@ -1,13 +1,26 @@
 package com.gelisam.failfast.generator
 
-
 /**
  * Parses an html file into a list of method signatures.
- * 
- * {{{
- * >>> 2+1
- * 3
- * }}}
  */
 object ScaladocParser {
+  val root = scala.xml.XML.loadFile("www.scala-lang.org/api/2.11.7/scala/collection/mutable/HashSet.html")
+  val foreachNode = (root \\ "li").filter(_ \@ "name" == "scala.collection.mutable.HashSet#foreach")(0)
+  
+  /**
+   * The signature of HashSet.foreach, encoded as XML.
+   * 
+   * {{{
+   * >>> ScaladocParser.signatureNode
+   * NodeSeq(<h4 class="signature">
+   *       <span class="modifier_kind">
+   *         <span class="modifier"/>
+   *         <span class="kind">def</span>
+   *       </span>
+   *       <span class="symbol">
+   *         <span class="name">foreach</span><span class="params">(<span name="f">f: (<span name="scala.collection.IterableLike.A" class="extype">A</span>) ⇒ <a name="scala.Unit" class="extype" href="../../Unit.html">Unit</a></span>)</span><span class="result">: <a name="scala.Unit" class="extype" href="../../Unit.html">Unit</a></span>
+   *       </span>
+   *       </h4>)
+   */
+  val signatureNode = (foreachNode \ "h4").filter(_ \@ "class" == "signature")
 }
