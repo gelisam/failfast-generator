bd57e7123727bbb2259f52be0095f829031ab344
Samuel Gélineau <gelisam+github@gmail.com>
2015-09-03 09:08:29 -0400

    Revert "Reader.map and Reader.filter"
    This reverts commit c61a6c5ce808ebb13e44df829bfd06fea84f9120.
    
    I am reverting the Reader.filter and Reader.map implementations because
    even though they look generally useful, I won't be needing them in this
    project. If I even need them in the future, they'll be in the history.

--- src/main/scala/com/gelisam/parser.scala
+++ src/main/scala/com/gelisam/parser.scala
@@ -3,8 +3,7 @@ package com.gelisam.parser
 import scala.util.parsing.input._
 
 /**
- * Have your Reader extend this to gain a more useful toString, as well as the
- * ability to map and filter.
+ * Have your Reader extend this to gain a more useful toString.
  * 
  * {{{
  * >>> import com.gelisam.parser.xml._
@@ -12,20 +11,6 @@ import scala.util.parsing.input._
  * // toString lists the remaining elements
  * >>> XmlTokenReader(<parent><child/></parent>)
  * XmlTokenReader(XmlOpen(<parent><child/></parent>),XmlOpen(<child/>),XmlClose(<child/>),XmlClose(<parent><child/></parent>))
- * 
- * >>> XmlTokenReader(<parent><child/></parent>) map {
- * ...   case XmlOpen(_) => XmlOpen(<open/>)
- * ...   case XmlClose(_) => XmlClose(<close/>)
- * ...   case token => token
- * ... }
- * XmlTokenReader(XmlOpen(<open/>),XmlOpen(<open/>),XmlClose(<close/>),XmlClose(<close/>))
- * }}}
- * 
- * >>> XmlTokenReader(<parent><child/></parent>) filter {
- * ...   case XmlOpen(_) => true
- * ...   case _ => false
- * ... }
- * XmlTokenReader(XmlOpen(<parent><child/></parent>),XmlOpen(<child/>))
  * }}}
  */
 trait ReaderUtil[A] extends Reader[A] {
@@ -35,43 +20,12 @@ trait ReaderUtil[A] extends Reader[A] {
     if (atEnd) Nil
     else first :: rest.toList
   
-  def className: String =
-    getClass.getName.split("\\.").last
-  
   // simplify debugging by listing all the remaining elements
   override def toString: String =
     {
+      val qualifiedName: String = getClass.getName
+      val shortName: String = qualifiedName.split("\\.").last
       val elems = toList mkString ","
-      s"${className}(${elems})"
+      s"${shortName}(${elems})"
     }
-  
-  def map[B](f: A => B): ReaderUtil[B] =
-    new MappedReader(this, f)
-  
-  def filter[B](p: A => Boolean): ReaderUtil[A] =
-    new FilteredReader(this, p)
-}
-
-class MappedReader[A,B](inner: ReaderUtil[A], f: A => B) extends ReaderUtil[B] {
-  def atEnd: Boolean = inner.atEnd
-  def first: B = f(inner.first)
-  def pos: Position = inner.pos
-  def rest: ReaderUtil[B] = new MappedReader(inner.rest, f)
-  
-  override def className: String =
-    inner.className
-}
-
-class FilteredReader[A](var inner: ReaderUtil[A], p: A => Boolean) extends ReaderUtil[A] {
-  while (!inner.atEnd && !p(inner.first)) {
-    inner = inner.rest
-  }
-  
-  def atEnd: Boolean = inner.atEnd
-  def first: A = inner.first
-  def pos: Position = inner.pos
-  def rest: ReaderUtil[A] = new FilteredReader(inner.rest, p)
-  
-  override def className: String =
-    inner.className
 }
