af0877503952b99cb4e840c19fd5113f5aeb9414
Samuel Gélineau <gelisam+github@gmail.com>
2015-09-05 21:43:50 -0400

    get the foreach node in one step

--- src/main/scala/com/gelisam/failfast/generator/ScaladocParser.scala
+++ src/main/scala/com/gelisam/failfast/generator/ScaladocParser.scala
@@ -25,24 +25,7 @@ case class MethodSig(name: String, parameters: List[Parameter], returnType: Type
  */
 object ScaladocParser extends XmlParsers {
   val root = XML.loadFile("www.scala-lang.org/api/2.11.7/scala/collection/mutable/HashSet.html")
-  val foreachNode = (root \\ "li").filter(_ \@ "name" == "scala.collection.mutable.HashSet#foreach")(0)
-  
-  /**
-   * The signature of HashSet.foreach, encoded as XML.
-   * 
-   * {{{
-   * >>> ScaladocParser.signatureNode
-   * NodeSeq(<h4 class="signature">
-   *       <span class="modifier_kind">
-   *         <span class="modifier"/>
-   *         <span class="kind">def</span>
-   *       </span>
-   *       <span class="symbol">
-   *         <span class="name">foreach</span><span class="params">(<span name="f">f: (<span name="scala.collection.IterableLike.A" class="extype">A</span>) ⇒ <a name="scala.Unit" class="extype" href="../../Unit.html">Unit</a></span>)</span><span class="result">: <a name="scala.Unit" class="extype" href="../../Unit.html">Unit</a></span>
-   *       </span>
-   *       </h4>)
-   */
-  val signatureNode = (foreachNode \ "h4").filter(_ \@ "class" == "signature")
+  val foreachNode = (root \\ "li").filter(_ \@ "name" == "scala.collection.mutable.HashSet#foreach").\("h4")(0)
   
   /**
    * Parse an XML type into a Type.
@@ -81,7 +64,7 @@ object ScaladocParser extends XmlParsers {
    * {{{
    * >>> ScaladocParser.parseAll(
    * ...   ScaladocParser.signatureParser,
-   * ...   ScaladocParser.signatureNode
+   * ...   ScaladocParser.foreachNode
    * ... ).get
    * def foreach(f: (A) => Unit): Unit
    * }}}
